1892d01fc7036169b7ccbedbebf4dd3293d4c635
ADITEYARAJ <58631474+ADITEYARAJ@users.noreply.github.com>
2020-01-18 18:23:03 +0530

    Add files via upload

---
+++ basic r/basic r.R
@@ -0,0 +1,20 @@
+a=4123 # cuyiujfjf
+a
+1:50
+print("hello")
+name <- "aadi"
+print(name)
+print(2+2)
+x <- 1:5
+x
+# X = 1:20
+typeof(X)
+y <- c(3,5,8,1,2)
+y
+typeof(y)
+a <- x + y
+a
+z <-3:9 
+z
+x * 2
+ls()
